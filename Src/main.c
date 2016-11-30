072c579c7bb658ad14218917439e435507202f12
Growlyj <296851292@qq.com>
2016-11-30 15:50:23 +0800

    修改开机图片为MD

--- Src/main.c
+++ Src/main.c
@@ -542,7 +542,7 @@ static void MX_IWDG_Init(void)
 {
   hiwdg.Instance = IWDG;
   hiwdg.Init.Prescaler = IWDG_PRESCALER_64;
-  hiwdg.Init.Reload = 1100;
+  hiwdg.Init.Reload = 2000;
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
   {
     Error_Handler();
